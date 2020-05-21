a4dcc567426c4627a3281c91773564542682c060
cx1964 <cx1964@mail.com>
2020-05-21 21:17:01 +0200

    aanpassing install libs

--- install_python_libs.sh
+++ install_python_libs.sh
@@ -5,15 +5,18 @@ source /c/tmp/cx1964ReposOTCSRestAPIs/env_python3_otcsRestAPIs/Scripts/activate
 # Linux 
 source ./env_python3_otcsRestAPIs/bin/activate
 
-
 # upgrade pip uitvoeren in Linux shell of Windows machine met mingw64
 python -m pip install --upgrade pip
 
 # install benodigde python libraries
 # uitvoeren in Linux shell of Windows machine met mingw64
 pip install requests
+
+# Ubuntu 18.04
+sudo apt-get install -y python3-yaml
+pip install pyyaml
+
 pip install json
-pip install yaml
 
 
 # toon geinstalleerde python libraries
